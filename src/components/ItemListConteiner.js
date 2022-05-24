ac4b38ec9a7df91fc3c6ceae35451bf2521cc5d1
lucho <luoronio@gmail.com>
2022-05-23 22:55:28 -0300

    fixed itemcount

--- src/components/ItemListConteiner.js
+++ src/components/ItemListConteiner.js
@@ -1,10 +1,13 @@
 import ItemCount from "./itemCount";
 function ItemListConteiner(props) {
+  const onAdd = (cantidad =>{
+    console.log('se agrego al carrito: ' +cantidad);
+  })
   console.log(props);
     return (
       <>
     <p> Texto de Prueba ItemListContainer {props.name}</p>
-    <ItemCount inicial="1" stock="5"/>     
+    <ItemCount inicial={1} stock={5} onAdd={onAdd}/>     
 
     </>
     );
--- src/components/itemCount.js
+++ src/components/itemCount.js
@@ -2,22 +2,38 @@ import InputGroup from 'react-bootstrap/InputGroup';
 import Button from 'react-bootstrap/Button';
 import FormControl from 'react-bootstrap/FormControl';
 import Form from 'react-bootstrap/Form';
-function ItemCount(props) {
+import { useState } from 'react';
+function ItemCount({inicial,stock,onAdd}) {
+    const [count,setCount]= useState(inicial);
+    const incrementar = ()=>{
+        if(stock >count){
+            setCount(count +1);
+        }
+        
+    }
+    const decrementar = ()=>{
+        if(count >1){
+            setCount(count -1);
+        }
+    }
+    const agregarCarrito =()=>{
+        onAdd(count);
+    }    
     return (
       <>
     <InputGroup className="mb-3">
-        <Button variant="outline-secondary" id="button-addon1">
+        <Button variant="outline-secondary" id="button-addon1" onClick={decrementar} >
         -
         </Button>
     <FormControl
       aria-label="Example text with button addon"
-      aria-describedby="basic-addon1" value={props.inicial} disabled
+      aria-describedby="basic-addon1" value={count} disabled
     />
-        <Button variant="outline-secondary" id="button-addon1">
+        <Button variant="outline-secondary" id="button-addon1" onClick={incrementar}>
             +
         </Button>
   </InputGroup>
-  <Button variant="outline-primary" id="button-addon1">
+  <Button variant="outline-primary" id="button-addon1" onClick={agregarCarrito}>
             agregar al carrito
         </Button>
 
